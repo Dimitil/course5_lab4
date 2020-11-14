e5b8d6761716e2d7141919d91e16273f6eb1c024
Dimitil <boikovdmitrii@gmail.com>
2020-11-14 15:48:35 +0300

    complete

--- human.cpp
+++ human.cpp
@@ -1,2 +1,47 @@
 #include "human.h"
 
+std::shared_ptr<human> human::child(const std::string& name,
+                                    std::shared_ptr<human>& mather,
+                                    std::shared_ptr<human>& father)
+{
+    auto child = std::make_shared<human>(name);
+
+    if(mather) {
+        child->m_mather = mather;
+        mather->m_childs.push_back(child);
+    }
+
+    if(father) {
+        child->m_father = father;
+        father->m_childs.push_back(child);
+    }
+
+    return child;
+}
+
+void human::printParents(size_t lvl) {
+    if(m_mather) {
+        m_mather->printParents(lvl + 1);
+        for(size_t i = 0; i < lvl; i++) std::cout<<'\t';
+        std::cout << '\t' << m_mather->m_name << '\n';
+       }
+    if(m_father) {
+        m_father->printParents(lvl + 1);
+        for(size_t i = 0; i < lvl; i++) std::cout<<'\t';
+        std::cout << '\t' << m_father->m_name << '\n';
+    }
+}
+
+void human::printChilds(size_t lvl) {
+    for (auto &el : m_childs) {
+        for(size_t i = 0; i < lvl; i++) std::cout<<'\t';
+        std::cout << el.lock() ->m_name << '\n';
+        el.lock()->printChilds(lvl+1);
+    }
+}
+
+void human::printTree() {
+   printParents();
+   printThis();
+   printChilds();
+}
--- human.h
+++ human.h
@@ -6,37 +6,6 @@
 #include <vector>
 #include <iostream>
 
-//Задание 6. shared_ptr и weak_ptr
-//Создаем генеалогическое дерево посредством класса human. В классе хранятся:
-//имя - string
-//возможно признак: жив или уже нет...
-//родители - shared_ptr (родители не всегда известны...)
-//дети - контейнер из weak_ptr (чтобы избежать циклических зависимостей)
-
-//Методы класса human:
-//конструктор - для инициализации имени и признака
-//конструктор копирования, оператор присваивания, move ???
-//статический метод child() -
-//				должен создать создать и вернуть обертку для родившегося человека
-//				+ сформировать все связи ребенка с родителями и наоборот
-
-//Ввести возможность распечатать генеалогическое дерево для указанного индивидума
-
-
-//История должна с кого-то начинаться => "Жили-были дед да баба, например, Адам и Ева"
-//(то есть на самом деле два деда и две бабы):
-
-
-//std::shared_ptr<human> grandM1(new human("Eva"));
-//...
-
-//у них появились дети - child():
-
-
-//а у детей в свою очередь свои дети:
-
-
-//...
 class human
 {
     std::string m_name;
@@ -52,54 +21,17 @@ public:
     human(human&& other) = delete;
     human& operator=(human&& other) = delete;
 
-    void printParents(size_t lvl = 0) {
-        if(m_mather) {
-            m_mather->printParents(lvl + 1);
-            for(size_t i = 0; i < lvl; i++) std::cout<<'\t';
-            std::cout << '\t' << m_mather->m_name << '\n';
-           }
-        if(m_father) {
-            m_father->printParents(lvl + 1);
-            for(size_t i = 0; i < lvl; i++) std::cout<<'\t';
-            std::cout << '\t' << m_father->m_name << '\n';
-        }
-    }
+    void printParents(size_t lvl = 0);
 
-    void printThis() {
-        std::cout << m_name << '\n' ;
-    }
-    void printChilds(size_t lvl = 1) {
-        for (auto &el : m_childs) {
-            for(size_t i = 0; i < lvl; i++) std::cout<<'\t';
-            std::cout << el.lock() ->m_name << '\n';
-            el.lock()->printChilds(lvl+1);
-        }
-    }
+    void printThis() { std::cout << m_name << '\n' ; }
 
-    void printTree() {
-       printParents();
-       printThis();
-       printChilds();
-    }
+    void printChilds(size_t lvl = 1);
+
+    void printTree();
 
     static std::shared_ptr<human> child(const std::string& name,
                                         std::shared_ptr<human>& mather,
-                                        std::shared_ptr<human>& father)
-    {
-        auto child = std::make_shared<human>(name);
-
-        if(mather) {
-            child->m_mather = mather;
-            mather->m_childs.push_back(child);
-        }
-
-        if(father) {
-            child->m_father = father;
-            father->m_childs.push_back(child);
-        }
-
-        return child;
-    }
+                                        std::shared_ptr<human>& father);
 };
 
 #endif // HUMAN_H
